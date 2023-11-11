583fc28a787bac662cb4789f9e7221d19c43c7d7
julian <jdrc@protonmail.com>
2023-11-11 18:33:54 -0500

    changed color scheme

--- script.js
+++ script.js
@@ -1,5 +1,27 @@
+const library = []
 let add_book = document.getElementById('add-book')
 
-add_book.addEventListener('click', () => {
-    alert('I forgot everythang!!!')
-})
+function Book(title, author, pages, read) {
+    this.title = title
+    this.author = author
+    this.pages = pages
+    this.read = read
+    this.haveRead = function() {
+        return !read
+    }
+}
+//  mockup books
+
+//  make it a pure function modding a (deep)copy of the library
+function addBook(library, book) {
+    const lib_copy = library // ?
+    lib_copy.push(book)
+    return lib_copy
+}
+
+//  looping func
+function showLibrary(library) {
+    for (let i = 0; i < library.length; i++) {
+        console.table(library[i])
+    }
+}
