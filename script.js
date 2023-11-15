77005eab2ffb2cb3506504883b6cdf900341381a
julian <jdrc@protonmail.com>
2023-11-14 19:54:35 -0500

    basic library working / fixed input validation

--- script.js
+++ script.js
@@ -1,14 +1,36 @@
 
 let add_book = document.querySelector('.add-book')
 let form_modal = document.querySelector('.form-modal')
+let form = document.querySelector('.form')
 let submit = document.querySelector('.submit')
 let title = document.getElementById('title')
 let author = document.getElementById('author')
 let pages = document.getElementById('pages')
-let title_val
-let author_val
-let pages_val
+let read_check = document.querySelector('.checkbox')
+let title_val = ''
+let author_val = ''
+let pages_val = 0
+let is_read = false
 const library = []
+
+function Book(title, author, pages, read) {
+    this.title = title
+    this.author = author
+    this.pages = pages
+    this.have_read = read
+    this.haveRead = function() {
+        return !read
+    }
+}
+
+function reset() {
+    title_val = ''
+    author_val = ''
+    pages_val = 0
+    is_read = false
+    form.reset()
+    form_modal.close()
+}
 
 document.body.addEventListener('keydown', e => {
     if (e.key == 'Escape') form_modal.close()
@@ -26,24 +48,23 @@ pages.addEventListener('input', () => {
     pages_val = pages.value
 })
 
+read_check.addEventListener('change', () => {
+    is_read = read_check.checked ? true : false
+})
+
 add_book.addEventListener('click', () => {
     form_modal.show()
 })
 
-submit.addEventListener('click', e => {
+form.addEventListener('submit', e => {
     e.preventDefault()
-    console.log(title_val)
+    let book = new Book(title_val, author_val, pages_val, is_read)
+    addBook(library, book)
+    showLibrary(library)
+    reset()
+    console.log(library)
 })
 
-// function Book(title, author, pages, read) {
-//     this.title = title
-//     this.author = author
-//     this.pages = pages
-//     this.have_read = read
-//     this.haveRead = function() {
-//         return !read
-//     }
-// }
 // //  mock books
 // let book1 = new Book(
 //     'Infinite Jest',
@@ -63,16 +84,23 @@ submit.addEventListener('click', e => {
 // let new_lib = addBook(library)
 // showLibrary(new_lib)
 
+//  Impure
+function addBook(library, book) {
+    if (book) library.push(book)
+    else return
+    // return library
+}
+
+//  Pure
 // function addBook(library, book) {
 //     const lib_copy = [...library] 
 //     book && lib_copy.push(book)  
 //     return lib_copy
 // }
 
-// //  looping func
-// function showLibrary(library) {
-//     for (let i = 0; i < library.length; i++) {
-//         console.table(library[i])
-//     }
-//     // console.table(library)
-// }
+//  looping func
+function showLibrary(library) {
+    for (let i = 0; i < library.length; i++) {
+        console.table(library[i])
+    }
+}
