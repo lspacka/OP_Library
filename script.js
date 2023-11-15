f8c0449edaf36093c46a0c07329da75849ba82e5
julian <jdrc@protonmail.com>
2023-11-15 17:38:39 -0500

    functional book cards

--- script.js
+++ script.js
@@ -1,15 +1,17 @@
 
 let add_book = document.querySelector('.add-book')
 let form_modal = document.querySelector('.form-modal')
+let book_modal = document.querySelector('.book-modal')
+let book_container = document.querySelector('.book-container')
 let form = document.querySelector('.form')
 let submit = document.querySelector('.submit')
-let title = document.getElementById('title')
-let author = document.getElementById('author')
-let pages = document.getElementById('pages')
+let title_input = document.getElementById('title')
+let author_input = document.getElementById('author')
+let pages_input = document.getElementById('pages')
 let read_check = document.querySelector('.checkbox')
-let title_val = ''
-let author_val = ''
-let pages_val = 0
+let title = ''
+let author = ''
+let pages = 0
 let is_read = false
 const library = []
 
@@ -23,10 +25,40 @@ function Book(title, author, pages, read) {
     }
 }
 
+function createBookCard() {
+    let card = document.createElement('div')
+    let card_btns = document.createElement('div')
+    let book_title = document.createElement('p')
+    let book_author = document.createElement('p')
+    let book_pages = document.createElement('p')
+    let read_btn = document.createElement('button')
+    read_btn.setAttribute('class', 'card-btn')
+    read_btn.setAttribute('id', 'read-btn')
+    let del_btn = document.createElement('button')
+    del_btn.setAttribute('class', 'card-btn')
+    del_btn.setAttribute('id', 'del-btn')
+
+    book_title.textContent = `"${title}"`
+    book_author.textContent = `${author}`
+    book_pages.textContent = `${pages} pages`
+    read_btn.textContent = 'Read'
+    del_btn.textContent = 'Remove'
+
+    card.appendChild(book_title)
+    card.appendChild(book_author)
+    card.appendChild(book_pages)
+    card_btns.appendChild(read_btn)
+    card_btns.appendChild(del_btn)
+    card_btns.setAttribute('class', 'card-btns')
+    card.setAttribute('class', 'card')
+    card.appendChild(card_btns)
+    book_container.appendChild(card)
+}
+
 function reset() {
-    title_val = ''
-    author_val = ''
-    pages_val = 0
+    title = ''
+    author = ''
+    pages = 0
     is_read = false
     form.reset()
     form_modal.close()
@@ -36,16 +68,16 @@ document.body.addEventListener('keydown', e => {
     if (e.key == 'Escape') form_modal.close()
 })
 
-title.addEventListener('input', () => {
-    title_val = title.value
+title_input.addEventListener('input', () => {
+    title = title_input.value
 })
 
-author.addEventListener('input', () => {
-    author_val = author.value
+author_input.addEventListener('input', () => {
+    author = author_input.value
 })
 
-pages.addEventListener('input', () => {
-    pages_val = pages.value
+pages_input.addEventListener('input', () => {
+    pages = pages_input.value
 })
 
 read_check.addEventListener('change', () => {
@@ -58,31 +90,13 @@ add_book.addEventListener('click', () => {
 
 form.addEventListener('submit', e => {
     e.preventDefault()
-    let book = new Book(title_val, author_val, pages_val, is_read)
+    let book = new Book(title, author, pages, is_read)
     addBook(library, book)
     showLibrary(library)
+    createBookCard()
     reset()
     console.log(library)
 })
-
-// //  mock books
-// let book1 = new Book(
-//     'Infinite Jest',
-//     'David Foster Wallace',
-//     '1079 pages',
-//     'false'
-// )
-
-// let book2 = new Book(
-//     'Smashing Node',
-//     'Guillermo Rauch',
-//     '322',
-//     'false'
-// )
-
-// library.push(book1, book2)
-// let new_lib = addBook(library)
-// showLibrary(new_lib)
 
 //  Impure
 function addBook(library, book) {
@@ -98,7 +112,6 @@ function addBook(library, book) {
 //     return lib_copy
 // }
 
-//  looping func
 function showLibrary(library) {
     for (let i = 0; i < library.length; i++) {
         console.table(library[i])
